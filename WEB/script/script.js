a739ad9616f41d01f0296c2ca0d8d1a7bfdae117
mahersan <Matheo.HERSAN@etu.uca.fr>
2023-01-06 13:44:14 +0100

    Modif web

---
+++ WEB/script/script.js
@@ -0,0 +1,44 @@
+// Sélectionne tous les éléments de la classe "card" et "card2" dans le document
+const cords = document.querySelectorAll('.card, .card2')
+
+// Pour chaque élément de la classe "card" ou "card2", ajoute un écouteur d'événement "click" qui appelle la fonction flipCard
+cords.forEach(card => {
+    card.addEventListener('click', flipCard);
+});
+
+// Fonction qui retourne une carte (élément de la classe "card" ou "card2") lorsqu'elle est cliquée
+function flipCard(){
+    // Récupère les éléments de la classe "front" et "back" à l'intérieur de l'élément sur lequel l'événement "click" a été déclenché
+    const front = this.querySelector('.card-side.front');
+    const back = this.querySelector('.card-side.back');
+    // Récupère les éléments de la classe "front2" et "back2" à l'intérieur de l'élément sur lequel l'événement "click" a été déclenché
+    const front2 = this.querySelector('.card-side.front2');
+    const back2 = this.querySelector('.card-side.back2');
+
+    // Si l'élément de la classe "front" existe, retourne la carte
+    if(front){
+        // Si la propriété "transform" de l'élément de la classe "front" est égale à "rotateY(180deg)", remet la carte à sa position initiale
+        if(front.style.transform === 'rotateY(180deg)'){
+            front.style.transform = 'rotateY(0deg)';
+            back.style.transform = 'rotateY(-180deg)';
+            // Sinon, retourne la carte
+        } else{
+            front.style.transform = 'rotateY(180deg)';
+            back.style.transform = 'rotateY(0deg)'
+        }
+    }
+
+    // Si l'élément de la classe "front2" existe, retourne la carte
+    if(front2){
+        // Si la propriété "transform" de l'élément de la classe "front2" est égale à "rotateY(180deg)", remet la carte à sa position initiale
+        if(front2.style.transform === 'rotateY(180deg)'){
+            front2.style.transform = 'rotateY(0deg)';
+            back2.style.transform = 'rotateY(-180deg)';
+            // Sinon, retourne la carte
+        } else{
+            front2.style.transform = 'rotateY(180deg)';
+            back2.style.transform = 'rotateY(0deg)'
+        }
+    }
+
+}
